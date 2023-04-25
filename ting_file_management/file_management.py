59a09422aab24952e5120c7bb376c60381ea0883
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-04-25 16:51:24 -0300

    requisito 2 concluído com sucesso

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith("txt"):
+        sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file, "r") as file:
+            lines = file.readlines()
+        output = [line.rstrip("\n") for line in lines]
+        return output
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
